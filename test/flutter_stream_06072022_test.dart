119ab7c2db877aa0b57f990af44ab80ed611d7a3
phamtanphat <phatdroid94@gmail.com>
2022-08-17 19:10:30 +0700

    update

--- test/flutter_stream_06072022_test.dart
+++ test/flutter_stream_06072022_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_stream_06072022/flutter_stream_06072022.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
 }
