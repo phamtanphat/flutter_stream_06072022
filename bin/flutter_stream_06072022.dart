1a2c74b368e4912f214ae419afb9c3b9813153c1
phamtanphat <phatdroid94@gmail.com>
2022-08-17 19:05:10 +0700

    Initial commit

---
+++ bin/flutter_stream_06072022.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_stream_06072022/flutter_stream_06072022.dart' as flutter_stream_06072022;
+
+void main(List<String> arguments) {
+  // Tao ra stream
+
+  // 1: Thông qua future
+  // var streamFuture = Stream.fromFuture(
+  //     Future.delayed(Duration(seconds: 2), () => 2)
+  // ).asBroadcastStream();
+  //
+  // streamFuture.listen((event) {
+  //   print(event);
+  // });
+  //
+  // streamFuture.listen((event) {
+  //   print(event);
+  // });
+
+  // 2: Interval
+  var streamInterval = Stream.periodic(Duration(seconds: 1), (index) => index);
+
+  streamInterval.take(20).listen((event) {
+    print(event);
+  });
+}
---
+++ test/flutter_stream_06072022_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_stream_06072022/flutter_stream_06072022.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
