44cacccb47d1d1cf9a5c4adb4b469ac3cc52f926
phamtanphat <phatdroid94@gmail.com>
2022-08-19 18:00:11 +0700

    behavior

--- lib/flutter_stream_06072022.dart
+++ lib/flutter_stream_06072022.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:rxdart/rxdart.dart';
+
 void main(List<String> arguments) {
   // Tao ra stream
 
@@ -24,15 +26,33 @@ void main(List<String> arguments) {
   // });
 
   // 3: StreamController
-  StreamController<String> nameController = StreamController();
-
-  nameController.stream.listen((event) {
-    print(event);
-  });
-
-  Future.delayed(Duration(seconds: 2), () {
-    nameController.sink.add("Pham Tan Phat");
-  });
+  // StreamController<String> nameController = StreamController();
+  //
+  // nameController.stream.listen((event) {
+  //   print(event);
+  // });
+  //
+  // Future.delayed(Duration(seconds: 2), () {
+  //   nameController.sink.add("Pham Tan Phat");
+  // });
 
   // BehaviorSubject, PublishSubject
+
+  var behaviorSubject = BehaviorSubject<int>();
+
+  behaviorSubject.listen((value) {
+    print("Current time 1: $value");
+  });
+
+  behaviorSubject.add(0);
+  behaviorSubject.add(1);
+  behaviorSubject.add(2);
+
+  behaviorSubject.listen((value) {
+    print("Current time 2: $value");
+  });
+
+  behaviorSubject.add(3);
+  behaviorSubject.add(4);
+  behaviorSubject.add(5);
 }
